09dea5ec7227cc4da941737302105a3bc805f85e
gustavo.olliveira49@gmail.com <gustavo.olliveira49@gmail.com>
2025-07-24 15:02:19 -0300

    Finalizando primeiro mini projeto de tabelas

--- angular-studies/src/app/pages/firstProject/component11.ts
+++ angular-studies/src/app/pages/firstProject/component11.ts
@@ -1,11 +1,65 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { Person } from 'src/app/model/Person';
 
 @Component({
   selector: 'app-component11',
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './component11.html',
   styleUrl: './component11.css'
 })
 export class Component11 {
 
+  form = new FormGroup({
+    name: new FormControl('', [Validators.required, Validators.minLength(3)]),
+    age: new FormControl(Number.NaN, [Validators.required, Validators.min(0), Validators.max(120)]),
+    city: new FormControl('', [Validators.required, Validators.minLength(3)])
+  });
+
+  btnRegister: boolean = true;
+
+  persons: Person[] = [];
+
+  selectPersonIndex: number = -1; 
+
+  registerPerson = () => {
+
+    this.persons.push(this.form.value as Person);
+
+    this.form.reset();
+
+    //console.table(this.persons);
+  }
+
+  selectPerson = (index: number) => {
+
+    this.selectPersonIndex = index;
+
+    this.form.setValue({
+      name: this.persons[index].name,
+      age: this.persons[index].age,
+      city: this.persons[index].city
+    });
+
+    this.btnRegister = false;
+  }
+
+  alterPerson = () => {
+    this.persons[this.selectPersonIndex] = this.form.value as Person;
+
+    this.cancelPerson();
+  }
+
+  removePerson = () => {
+    this.persons.splice(this.selectPersonIndex, 1);
+
+     this.cancelPerson();
+  }
+
+  cancelPerson = () => {
+    this.form.reset()
+
+    this.btnRegister = true;
+    this.selectPersonIndex = -1;
+  }
 }
--- angular-studies/src/app/pages/studies/component00.ts
+++ angular-studies/src/app/pages/studies/component00.ts
@@ -1,14 +1,15 @@
 import { Component } from '@angular/core';
-import { Component01 } from "../../components/textInterpolation/component01";
-import { Component02 } from "../../components/eventBinding/component02";
-import { Component03 } from "../../components/propertyBinding/component03";
-import { Component04 } from "../../components/ngIf/component04";
-import { Component05 } from "../../components/ngFor/component05";
-import { Component06 } from "../../components/ngSwitch/component06";
-import { Component07 } from "../../components/ngStyle-ngClass/component07";
-import { Component08 } from "../../components/controlFlow/component08";
-import { Component09 } from "../../components/formsModule/component09";
-import { Component10 } from "../../components/reactiveForms/component10";
+import { Component01 } from '../../components/textInterpolation/component01';
+import { Component02 } from '../../components/eventBinding/component02';
+import { Component03 } from '../../components/propertyBinding/component03';
+import { Component05 } from '../../components/ngFor/component05';
+import { Component04 } from '../../components/ngIf/component04';
+import { Component06 } from '../../components/ngSwitch/component06';
+import { Component07 } from '../../components/ngStyle-ngClass/component07';
+import { Component08 } from '../../components/controlFlow/component08';
+import { Component09 } from '../../components/formsModule/component09';
+import { Component10 } from '../../components/reactiveForms/component10';
+
 
 @Component({
   selector: 'app-component00',
